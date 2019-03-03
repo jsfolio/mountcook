943359a45cbfe7db307b25aef8d17b55f1ec4d7d
jsfolio <47778599+jsfolio@users.noreply.github.com>
2019-03-03 23:02:22 +0900

    update

---
+++ html/js/mobile_menu.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    var chk = false;
+    $('.menu_box').click(function(){
+        if(chk == true) { //메뉴닫기
+            $('.menu_box').removeClass('active'); 
+            $('.menu_box').addClass('menu_box_reverse');  
+            $('.panel-overlay').css({
+                display : 'none'
+            })
+            chk = false; 
+        }
+        else { //메뉴열기
+            $('.menu_box').removeClass('menu_box_reverse');
+            $('.menu_box').addClass('active');
+            $(".mobile_menu").removeClass("hide");
+            $('.panel-overlay').css({
+                display : 'block'
+            })
+            chk = true;
+
+        }
+
+       $(".mobile_menu").toggleClass("show");
+    });
+  
+
+    $('.panel-overlay').click(function(){
+        $('.menu_box').removeClass('active'); 
+        $('.menu_box').addClass('menu_box_reverse');  
+        $(".mobile_menu").toggleClass("show");
+        $('.panel-overlay').css({
+            display : 'none'
+        })
+        chk = false; 
+            
+    });
+
+});
